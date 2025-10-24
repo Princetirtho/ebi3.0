ac2ca66b028d7beae50c6bdc4ac353b7da88ed8b
Princetirtho <163094969+Princetirtho@users.noreply.github.com>
2025-10-24 14:55:13 +0600

    Add files via upload

---
+++ class11-12/firebase-config.js
@@ -0,0 +1,16 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDnG7WhZTCHPvz2sqS4KkPmZFOX9XxpttQ",
+  authDomain: "private-chat-550ab.firebaseapp.com",
+  databaseURL: "https://private-chat-550ab-default-rtdb.firebaseio.com",
+  projectId: "private-chat-550ab",
+  storageBucket: "private-chat-550ab.firebasestorage.app",
+  messagingSenderId: "108768473913",
+  appId: "1:108768473913:web:a57cc49c0103f57467eee8",
+  measurementId: "G-YJRLY8TXMT"
+};
+
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
